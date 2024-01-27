b5a367bf200042a717349d21add6edef7bdf9fd1
Hercogs <jecuks96@gmail.com>
2024-01-27 12:00:33 +0000

    fix: updates controller parameter to better reach goal

---
+++ path_planner_server/launch/map_server.launch.py
@@ -0,0 +1,36 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    package_name = "path_planner_server"
+    
+    nav2_yaml = os.path.join(get_package_share_directory(package_name), 'config', 'amcl_config.yaml')
+    map_file = os.path.join(get_package_share_directory('cartographer_slam'), 'maps', 'sim_map.yaml')
+
+    return LaunchDescription([
+        Node(
+            package='nav2_map_server',
+            executable='map_server',
+            name='map_server',
+            output='screen',
+            parameters=[{'use_sim_time': True}, 
+                        {"topic_name": "map"},
+                        {"frame_id": "map"},
+                        {'yaml_filename': map_file}]
+        ),
+
+
+        Node(
+            package='nav2_lifecycle_manager',
+            executable='lifecycle_manager',
+            name='lifecycle_manager_map_server',
+            output='screen',
+            parameters=[{'use_sim_time': True},
+                        {'autostart': True},
+                        {'node_names': ['map_server']}]
+        )
+    ])
